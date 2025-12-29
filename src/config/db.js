7e885689ccad2c66654433b3d903f9b03068bcd3
Konduru Tharun <kondurutharun8@gmail.com>
2025-12-29 22:32:29 +0530

    Configure PostgreSQL for Render

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "job_tracker",
-    password: "hello2005",
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
